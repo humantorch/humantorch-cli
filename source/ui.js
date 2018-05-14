2da45ae356d91eacdd158c96276c33d37c0f3886
Scott Kosman <kosman.scott@gmail.com>
2018-05-14 15:03:17 +0200

    removing stray comments

--- source/ui.js
+++ source/ui.js
@@ -1,9 +1,7 @@
 'use strict';
-// const path = require('path');
 const {h, Text} = require('ink');
 const SelectInput = require('ink-select-input');
 const opn = require('opn');
-// const terminalImage = require('terminal-image');
 
 const open = url => opn(url, {wait: false});
 
@@ -42,35 +40,14 @@ const items = [
 		label: 'CV',
 		url: 'https://stackoverflow.com/cv/scottkosman'
 	},
-	// {
-	// 	label: 'Blog',
-	// 	url: 'https://blog.sindresorhus.com'
-	// },
-	// {
-	// 	label: 'Ask Me Anything',
-	// 	url: 'https://github.com/sindresorhus/ama'
-	// },
 	{
 		label: 'Contact',
 		url: 'http://prayingmadness.com/contact'
 	},
-	// {
-	// 	label: 'Support my open source work',
-	// 	url: 'https://www.patreon.com/sindresorhus'
-	// },
-	// {
-	// 	label: 'Unicorns!',
-	// 	async action() {
-	// 		console.log(await terminalImage.file(path.join(__dirname, 'unicorn1.gif')));
-	// 		console.log(await terminalImage.file(path.join(__dirname, 'unicorn2.gif')));
-	// 		console.log(await terminalImage.file(path.join(__dirname, 'unicorn3.gif')));
-	// 	}
-	// },
-	// TODO: Add separator item here when https://github.com/vadimdemedes/ink-select-input/issues/4 is done
 	{
 		label: 'Quit',
 		action() {
-			process.exit(); // eslint-disable-line unicorn/no-process-exit
+			process.exit(); 
 		}
 	}
 ];
