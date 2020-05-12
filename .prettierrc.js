1b166dc978320c2169f7e87af572c9a7d68e5fa5
Scott Kosman <kosman.scott@gmail.com>
2020-05-12 09:58:15 -0400

    THRILLWORKS update

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	printWidth: 10000,
+	tabWidth: 2,
+	bracketSpacing: true,
+	singleQuote: true,
+	trailingComma:
+		'all',
+};
--- source/ui.js
+++ source/ui.js
@@ -1,7 +1,7 @@
-"use strict";
-const { h, Text } = require("ink");
-const SelectInput = require("ink-select-input");
-const opn = require("opn");
+'use strict';
+const { h, Text } = require('ink');
+const SelectInput = require('ink-select-input');
+const opn = require('opn');
 
 const open = (url) => opn(url, { wait: false });
 
@@ -17,37 +17,39 @@ const handleSelect = (item) => {
 
 const items = [
 	{
-		label: "LinkedIn – feel free to join my Professional Network™!",
-		url: "https://www.linkedin.com/in/scottkosman",
+		label: 'LinkedIn – feel free to join my Professional Network™!',
+		url: 'https://www.linkedin.com/in/scottkosman',
 	},
 	{
-		label: "Some past projects – a bit out of date, maybe someday though!",
-		url: "http://prayingmadness.github.io",
+		label: 'Some past projects – a bit out of date, maybe someday though!',
+		url: 'http://prayingmadness.github.io',
 	},
 	{
-		label:
-			"How To Scott – useful reading if you currently do or someday want to work for/with me",
-		url: "https://github.com/humantorch/how-to-scott",
+		label: 'How To Scott – useful reading if you currently do or someday want to work for/with me',
+		url: 'https://github.com/humantorch/how-to-scott',
 	},
 	{
-		label:
-			"Twitter – mostly telling jokes and complaining about the Edmonton Oilers",
-		url: "https://twitter.com/humantorch",
+		label: 'THRILLWORKS – where I work',
+		url: 'https://thrillworks.com',
+	},
+	{
+		label: 'Twitter – mostly telling jokes and complaining about the Edmonton Oilers',
+		url: 'https://twitter.com/humantorch',
 	},
 	{
 		label: "GitHub – if you're reading this you know what GitHub is",
-		url: "https://github.com/humantorch",
+		url: 'https://github.com/humantorch',
 	},
 	{
-		label: "Instagram – see what I had for lunch!",
-		url: "https://instagram.com/humantorch",
+		label: 'Instagram – see what I had for lunch!',
+		url: 'https://instagram.com/humantorch',
 	},
 	{
-		label: "CV - see what I used to do",
-		url: "https://stackoverflow.com/cv/scottkosman",
+		label: 'CV - see what I used to do',
+		url: 'https://stackoverflow.com/cv/scottkosman',
 	},
 	{
-		label: "Quit",
+		label: 'Quit',
 		action() {
 			process.exit();
 		},
@@ -58,13 +60,7 @@ module.exports = () => (
 	<div>
 		<br />
 		<div>
-			<Text>
-				Toronto-based Engineering Manager/Tech Director. Currently looking for a
-				new opportunity – open to Toronto or remote full-time opportunities, if
-				you're hiring then I'm your guy! A front-end developer who pivoted to
-				management back in 2014, I now enjoy leading teams building the next
-				generation of stuff you do on this "internet" thing. He/him.
-			</Text>
+			<Text>Toronto-based Engineering Director at THRILLWORKS, a marketing/adtech agency. A recovering front-end developer who pivoted to management back in 2014, I now enjoy leading teams building the next generation of stuff you do on this "internet" thing. He/him.</Text>
 		</div>
 		<br />
 		<SelectInput items={items} onSelect={handleSelect} />
